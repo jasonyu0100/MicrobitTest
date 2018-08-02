aaee5563e4428b45ecfd499e0a69a51142b29dae
Jason Yu <jasonyu0100@gmail.com>
2018-08-02 13:02:54 +1000

    new

--- microSerialCom.py
+++ microSerialCom.py
@@ -37,40 +37,12 @@ class TerminalControl:
         '''
         Get histories of microbits and calcualte claps from there
         '''
+        return
 
-
-class MicroBit:
-    def __init__(self):
-        '''
-        XYZ is located at user's chest
-        '''
-        self.x = 0
-        self.y = 0
-        self.z = 0
-
-    def applyAcceleration(self,x,y,z,time):
-        '''
-        Finds acceleration of all values at a given time and updates values
-        '''
-        self.x += x*time
-        self.y += y*time
-        self.z += z*time
-
-    def setXYZ(self,x,y,z):
-        '''
-        Sets XYZ directly through values
-        '''
-        self.x = x
-        self.y = y
-        self.z = z
-
-    def 
-
-        
-
-control = TerminalControl("COM5",[])
-control.serial.open()
-control.writeToConsole("Hello World")
+if __name__ == '__main__':
+    control = TerminalControl("COM5",[])
+    control.serial.open()
+    control.writeToConsole("Hello World")
 
     
 
---
+++ microbit.py
@@ -0,0 +1,24 @@
+class MicroBit:
+    def __init__(self):
+        '''
+        XYZ is located at user's chest
+        '''
+        self.x = 0
+        self.y = 0
+        self.z = 0
+
+    def applyAcceleration(self,x,y,z,time):
+        '''
+        Finds acceleration of all values at a given time and updates values
+        '''
+        self.x += x*time
+        self.y += y*time
+        self.z += z*time
+
+    def setXYZ(self,x,y,z):
+        '''
+        Sets XYZ directly through values
+        '''
+        self.x = x
+        self.y = y
+        self.z = z
