d3fc1cce93d9931674a218d27ee42c5767e9e7e0
sunnyyan01 <32755606+sunnyyan01@users.noreply.github.com>
2018-08-30 12:59:24 +1000

    Started node.js server code

--- server_send.py
+++ server_send.py
@@ -1,7 +1,8 @@
-import urllib.request
-from .microSerialCom import TerminalControl
+import urllib.request, urllib.parse
+from microSerialCom import TerminalControl
+from microbit import MicroBit
 
-serverURL = "127.0.0.1:3000"
+serverURL = "127.0.0.1:3000/receive_data"
 control = TerminalControl("COM5",MicroBit(),MicroBit())
 control.serial.open()
 while True:
@@ -13,4 +14,6 @@ while True:
         data_send["l"] = control.leftMicroBit.getXYZ()
     elif control.rightMicroBit.btnA:
         data_send["r"] = control.rightMicroBit.getXYZ()
+    data_send = urllib.parse.urlencode(data_send)
+    data_send = data.encode("ascii")
     urllib.request(serverURL,data=data_send)
